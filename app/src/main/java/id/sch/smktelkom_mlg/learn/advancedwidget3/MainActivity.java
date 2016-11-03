51bd5b24ed7336842826f4b55ca7f48e598f2a40
Mahardika Putri P <mahardika_pertiwi_23rpl@student.smktelkom-mlg.sch.id>
2016-11-03 10:35:14 +0700

    Simple Data Binding Final

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingutil.setContentView(this, R.layout.activity_main);
+        binding.setNama(nama);
+
+        
     }
 }
